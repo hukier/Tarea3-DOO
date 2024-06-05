49905dba8ab3797e0d4708cc544f8ad509bd812e
Roberto <pantostado1601@gmail.com>
2024-06-05 14:51:26 -0400

    added: png productos y de moneda

--- src/main/java/GUI/PanelPrincipal.java
+++ src/main/java/GUI/PanelPrincipal.java
@@ -10,7 +10,7 @@ public class PanelPrincipal extends JPanel {
         setLayout(null);
 
         panelComprador = new PanelComprador();
-        panelExpendedor = new PanelExpendedor();
+        //panelExpendedor = new PanelExpendedor();
 
 
 
