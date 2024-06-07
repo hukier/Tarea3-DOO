ef56b600419a868851f446a8ffd7877a30c7dcdb
Martin <martingonzalez2022@udec.cl>
2024-06-06 23:13:48 -0400

    Added: Inventario y PanelInventario // Clase semi vacia de Panel Inventario y funcionamiento general de Inventario

--- src/main/java/GUI/Inventario.java
+++ src/main/java/GUI/Inventario.java
@@ -1,10 +1,54 @@
 package GUI;
 
-public class Inventario {
+import javax.swing.JFrame;
+import java.awt.*;
 
-    public void modificar(int i) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'modificar'");
+public class Inventario extends JFrame {
+
+    public PanelInventario panelInventario;
+
+    /**
+     * Constructor de la clase Inventario.
+     */
+    public Inventario() {
+        panelInventario = new PanelInventario(4000);
+        this.add(panelInventario);
+        panelInventario.modificarInventario();
+    }
+
+    /**
+     * Metodo para mostrar la ventana de inventario.
+     */
+    public void mostrarPanel() {
+        this.setVisible(true);
+        this.setTitle("Inventario");
+        this.setResizable(false);
+        this.setSize(320, 350); // valores al azar
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.getContentPane().setBackground(Color.black);
+        this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+    }
+
+    public void modificar(int seleccion) { // Base de modificacion de los valores del inventario
+        switch (seleccion) {
+            case 1:
+                panelInventario.inv_cocacola++;
+                break;
+            case 2:
+                panelInventario.inv_fanta++;
+                break;
+            case 3:
+                panelInventario.inv_sprite++;
+                break;
+            case 4:
+                panelInventario.inv_snickers++;
+                break;
+            case 5:
+                panelInventario.inv_super8++;
+                break;
+        }
+        panelInventario.modificarInventario();
+
     }
 
 }
---
+++ src/main/java/GUI/PanelInventario.java
@@ -0,0 +1,21 @@
+package GUI;
+
+import javax.swing.JPanel;
+
+public class PanelInventario extends JPanel {
+
+    public int inv_cocacola = 0;
+    public int inv_fanta = 0;
+    public int inv_sprite = 0;
+    public int inv_snickers = 0;
+    public int inv_super8 = 0;
+
+    public PanelInventario(int i) {
+
+    }
+
+    public void modificarInventario() {
+
+    }
+
+}
